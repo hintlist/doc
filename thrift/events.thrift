47680438231db7444756779b367ff3ba8a8fad3d
Ksusha Minich <ksusha484@gmail.com>
2014-04-16 21:04:55 +0300

    added service in places.thrift

--- thrift/events.thrift
+++ thrift/events.thrift
@@ -5,15 +5,17 @@ include "map.thrift"
 
 typedef i32 int
 typedef i64 timestamp
+typedef string TTypeKey
+typedef string TEventKey
 
 struct EventType {
-	1: string key;
+	1: TTypeKey key;
 	2: string name;
 	3: int timestamp;
 }
 
 struct Event {
-	1: string key;
+	1: TEventKey key;
 	2: string name;
 	3: string typeKey;
 	4: string shortDescription;
@@ -22,4 +24,20 @@ struct Event {
 	7: int myRate;
 	8: string placeKey;
 	9: int timestamp;
+}
+
+service EventsManager {
+	list<Event> findAll(
+		1: int page,
+		2: int count
+	);
+	list<Event> findAllByType(
+		1: TTypeKey key,
+		2: int page,
+		3: int count
+	);
+	Event findByKey(
+		1: TEventKey key
+	);
+	
 }
